5bde465e438b2adf9767ce4fbdb667bc9326484f
vova <vladyegorin@gmail.com>
2024-12-07 15:54:14 -0800

    sendmenu and importance keyboard

--- src/main/java/vladyegorinco/Bot.java
+++ src/main/java/vladyegorinco/Bot.java
@@ -4,15 +4,28 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 public class Bot extends TelegramLongPollingBot {
     private String botToken;
+
+
+
+    private final InlineKeyboardButton redTag = InlineKeyboardButton.builder().text("🔴 - important").callbackData("red").build();
+    private final InlineKeyboardButton greenTag = InlineKeyboardButton.builder().text("🟢 - not important").callbackData("green").build();
+    private final InlineKeyboardMarkup keyboardImportanceTag = InlineKeyboardMarkup.builder().
+            keyboardRow(List.of(redTag)).
+            keyboardRow(List.of(greenTag)).
+            build();
+
 
     @Override
     public String getBotUsername() {
@@ -75,7 +88,10 @@ public class Bot extends TelegramLongPollingBot {
         if (msg.getText().equals("/start")) {
             sendText(id, "Welcome to Task Manager! \nSee what I can do by typing /help\n");
 
-        } else if (msg.getText().equals("/help")) {
+        } else if(msg.getText().equals("/addtask")){
+            sendMenu(id,"<b><i> Choose an importnace tag for your task</i></b>", keyboardImportanceTag);
+        }
+        else if (msg.getText().equals("/help")) {
             sendText(id, "/addtask - Add a task\n/removetask - Remove a task\n/showtasklist - Show list of tasks");
         } else if (msg.getText().equalsIgnoreCase("hello")  || msg.getText().equalsIgnoreCase("hi") ){
             sendText(id, "Hi there!");
@@ -93,6 +109,20 @@ public class Bot extends TelegramLongPollingBot {
         }
 
     }
+
+
+    public void sendMenu(Long who, String txt, InlineKeyboardMarkup kb){
+        SendMessage sm = SendMessage.builder().chatId(who.toString())
+                .parseMode("HTML").text(txt)
+                .replyMarkup(kb).build();
+
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     private void IDontUnderstand(Message msg, Long id) {
         sendText(id, "I don't understand. Try /help to see what I can do.");
